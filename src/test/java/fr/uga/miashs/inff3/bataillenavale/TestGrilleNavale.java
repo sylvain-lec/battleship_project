5f7020bc451f0f016e94a721e6f8e40b83e4bce4
Sylvain Lecire <7266910+sylvainlecire@users.noreply.github.com>
2023-12-11 17:23:23 +0100

    Ajout des tests manquants

---
+++ src/test/java/fr/uga/miashs/inff3/bataillenavale/TestGrilleNavale.java
@@ -0,0 +1,16 @@
+package fr.uga.miashs.inff3.bataillenavale;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestGrilleNavale {
+
+	public TestGrilleNavale() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/fr/uga/miashs/inff3/bataillenavale/TestNavire.java
@@ -0,0 +1,16 @@
+package fr.uga.miashs.inff3.bataillenavale;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestNavire {
+
+	public TestNavire() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
