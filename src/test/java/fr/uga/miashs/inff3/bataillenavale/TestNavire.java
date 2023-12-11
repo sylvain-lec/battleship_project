8b6e63b93406e7e9635afc5b571556eb1dad675f
Vouzanvello <143947619+Vouzanvello@users.noreply.github.com>
2023-12-11 18:06:15 +0100

    TestNavire 1

--- src/test/java/fr/uga/miashs/inff3/bataillenavale/TestNavire.java
+++ src/test/java/fr/uga/miashs/inff3/bataillenavale/TestNavire.java
@@ -2,12 +2,67 @@ package fr.uga.miashs.inff3.bataillenavale;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
+
 public class TestNavire {
 
-	public TestNavire() {
-		// TODO Auto-generated constructor stub
+	@Test
+	public void testConstructeurNavire() {
+		
 	}
+	
+	@Test
+	public void testToString() {
+		
+	}	
+	
+	@Test
+	public void testCoordonneeGetDebut() {
+		
+	}
+	
+	@Test
+	public void testCoordonneeGetFin() {
+		
+	}
+	
+	@Test
+	public void testContient() {
+		
+	}
+	
+	@Test
+	public void testTouche() {
+		
 
+	}
+	
+	@Test
+	public void testChevauche() {
+		
+	}
+	
+	@Test
+	public void testRecoitTir() {
+		
+	}
+	
+	@Test
+	public void testEstToucheCoordonnee() {
+		
+	}
+	
+	@Test
+	public void testEstTouche() {
+		
+	}
+	
+	@Test
+	public void testEstCoule() {
+		
+	}
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
