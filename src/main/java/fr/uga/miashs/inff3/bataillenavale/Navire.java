c3427bdeaca5fc39e8a49ab9be3feed415d41c63
Sylvain Lecire <7266910+sylvainlecire@users.noreply.github.com>
2023-12-11 18:46:14 +0100

    Ilias + Aurore = fail

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/Navire.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/Navire.java
@@ -7,19 +7,35 @@ public class Navire {
 	private Coordonnee[] partiesTouchees;
 	private int nbTouchees; 
 
-	public Navire(Coordonnee debut,int longueur,boolean estVertical){
+	public Navire(Coordonnee debut, int longueur, boolean estVertical){
 		this.debut = debut;
-		if (estVertical==true) 
+		// si vertical, ligne impactée par longueur sinon c'est la colonne
+		if (estVertical) 
 			this.fin = new Coordonnee (this.debut.getLigne() + (longueur - 1), this.debut.getColonne());
 		else
 			this.fin = new Coordonnee (this.debut.getLigne(), this.debut.getColonne() + (longueur - 1));
-		
+	}
+	
+	public boolean estVertical() {
+		// test si navire est vertical = true
+		if (debut.getLigne() == fin.getLigne())
+			return false;
+		return true;		
 	}
 
 	public String toString() {
-		String position;
-		if()
-		return null;
+		int longueur;
+		String orientation = "";
+		if (!estVertical()) {
+			longueur = fin.getColonne() - debut.getColonne();
+			orientation += "vertical";
+		}
+		else {
+			longueur = fin.getLigne() - debut.getLigne();
+			orientation += "horizontal";
+		}
+		// retourne un résultat de type "Navire(B1, 4, horizontal)"
+		return ("Navire(" + debut.toString() + ", " + longueur + ", " + orientation + ")");
 	}
 	
 	public Coordonnee getDebut() {
@@ -31,15 +47,16 @@ public class Navire {
 	}
 	
 	public boolean contient(Coordonnee c) {
-		
-		if (this.debut.getColonne()==this.fin.getColonne()) {
-			if(c.getColonne()==this.debut.getColonne() && (c.getLigne()>=this.debut.getLigne()&& c.getLigne()<=this.fin.getLigne()))
-				return true;
-		}else {
-			if(c.getLigne()==this.debut.getLigne() && (c.getColonne()>= this.debut.getColonne() && c.getColonne()<= this.fin.getColonne()))
+		// si vertical, test si c sur même colonne que this et si ligne de c comprise entre début et fin de this
+		if (estVertical()) {
+			if (c.getColonne() == this.debut.getColonne() && (c.getLigne() >= this.debut.getLigne() && c.getLigne() <= this.fin.getLigne()))
 				return true;
 		}
-		
+		else {
+		// si horizontal, test si c sur même ligne que this et si colonne de c comprise entre début et fin de this
+			if(c.getLigne() == this.debut.getLigne() && (c.getColonne() >= this.debut.getColonne() && c.getColonne() <= this.fin.getColonne()))
+				return true;
+		}
 		return false;
 	}
 	
