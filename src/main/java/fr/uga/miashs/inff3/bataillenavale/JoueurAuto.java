d5e73fd2f90ba3c654d5342b79d3779d7d7b4b7e
honopio <honorine.piolat@gmail.com>
2023-12-12 19:48:09 +0100

    methodes joueur ambiance bizarre

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurAuto.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurAuto.java
@@ -12,16 +12,35 @@ public class JoueurAuto extends JoueurAvecGrille {
 	}
 
 	protected void retourAttaque(Coordonnee c, int etat) {
-			
+		// jsp ce qu'il faut faire ici j'ai fait un peu au hasard
+	
+		if (etat==4)
+			System.out.println("tir en " + c +" : gameover");
+		if (etat==1)
+			System.out.println("tir en " + c+" : navire touché");
+		if (etat==2)
+			System.out.println("tir en " + c+" : navire coulé");
+		if (etat==3)
+			System.out.println("tir en " + c+" : à l'eau");
 	}
 	
 	protected void retourDefense(Coordonnee c, int etat) {
-		
+		//affichage à la console des étapes de jeu
+		this.retourAttaque(c, etat);
 	}
 	
 	public Coordonnee choixAttaque() {
 		// genere coordonnee aleatoire
-		Coordonnee attaque = new Coordonnee(new Random().nextInt(super.getTailleGrille() - 1), new Random().nextInt(super.getTailleGrille() - 1));
+		
+		//version simple :
+		//Coordonnee attaque = new Coordonnee(new Random().nextInt(super.getTailleGrille() - 1), new Random().nextInt(super.getTailleGrille() - 1));
+		//return attaque;
+		
+		Coordonnee attaque;
+		do {
+			attaque = new Coordonnee(new Random().nextInt(super.getTailleGrille() - 1), new Random().nextInt(super.getTailleGrille() - 1));
+		} while (super.grille.estDansTirsRecus(attaque) || attaque.voisine(???)); //methode de superclasse estDansTirsRecus private
+		
 		return attaque;
-	}
+	
 }
--- src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurTexte.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurTexte.java
@@ -16,19 +16,34 @@ public class JoueurTexte extends JoueurAvecGrille {
 	protected void retourAttaque(Coordonnee c, int etat) {
 		// Réalise l'affichage à la console des étapes de jeu.
 		// c est la coordonnée à laquelle le tir a eu lieu et etat le résultat de l'attaque. etat ne peut être que TOUCHE, COULE, A_L_EAU, ou GAMEOVER.
-		
+		if (etat==4)
+			System.out.println("tir en " + c +" : gameover");
+		if (etat==1)
+			System.out.println("tir en " + c+" : navire touché");
+		if (etat==2)
+			System.out.println("tir en " + c+" : navire coulé");
+		if (etat==3)
+			System.out.println("tir en " + c+" : à l'eau");
 	}
 	
 	protected void retourDefense(Coordonnee c, int etat) {
-		
+		//c est la coordonnée à laquelle le tir a eu lieu et etat le résultat de ce tir. etat ne peut être que TOUCHE, COULE, A_L_EAU, ou GAMEOVER. 
+		this.retourAttaque(c, etat);
 	}
 	
 	public Coordonnee choixAttaque() {
+		Scanner sc = new Scanner(System.in);
 		// recueille au clavier la saisie de la coordonnée à attaquer
 		System.out.print("Coordonnées de l'attaque: ");
 		// enregistrement de l'input dans une nouvelle coordonnee
 		Coordonnee attaque = new Coordonnee(sc.nextLine());
-		return attaque;
+		return attaque; //CA RETOURNE RIEN
 	}
 
+	public static void main(String[] args) {
+		Coordonnee c = new Coordonnee("B3");
+		GrilleNavale g = new GrilleNavale(10,2);
+		JoueurTexte j = new JoueurTexte(g, "sylvain");
+		j.choixAttaque(); j.retourAttaque(c, 2);
+	}
 }
