f84a781df313f2fd47488cb4601ca0a85c1dca42
honopio <honorine.piolat@gmail.com>
2023-12-12 22:08:47 +0100

    nvl classe

---
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurAutoMoyen.java
@@ -0,0 +1,10 @@
+package fr.uga.miashs.inff3.bataillenavale;
+
+public class JoueurAutoMoyen extends JoueurAuto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
