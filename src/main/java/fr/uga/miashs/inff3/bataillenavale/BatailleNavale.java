6da06e1a47cab0715dbdede9bbedc333aab4cda4
Sylvain Lecire <7266910+sylvainlecire@users.noreply.github.com>
2023-12-13 13:05:25 +0100

    New class BatailleNavale

---
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/BatailleNavale.java
@@ -0,0 +1,43 @@
+package fr.uga.miashs.inff3.bataillenavale;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class BatailleNavale {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BatailleNavale window = new BatailleNavale();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public BatailleNavale() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
