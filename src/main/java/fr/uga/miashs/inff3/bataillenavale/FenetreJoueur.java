6c6f1f84361ad665bf6a28b2bd875e245b479570
Sylvain Lecire <7266910+sylvainlecire@users.noreply.github.com>
2023-12-13 12:30:40 +0100

    Fenetrejoueur

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/FenetreJoueur.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/FenetreJoueur.java
@@ -9,6 +9,8 @@ import javax.swing.border.EmptyBorder;
 public class FenetreJoueur extends JFrame {
 
 	private JPanel contentPane;
+	private GrilleGraphique grilleTirs;
+	private GrilleNavaleGraphique grilleDefense;
 
 	/**
 	 * Launch the application.
@@ -29,7 +31,26 @@ public class FenetreJoueur extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	/*public FenetreJoueur() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}*/
+	
 	public FenetreJoueur() {
+		this("Nom du joueur", 10);
+		}
+	
+	public FenetreJoueur(String nom, int taille) {
+		// permet d'obtenir une fenêtre pour un joueur de nom nom avec des grilles de taille taille. Un placement de navires automatique sur grilleDefense peut être réalisé.
+		grilleTirs = new GrilleGraphique(taille);
+		grilleDefense = new GrilleNavaleGraphique(taille);
+		JoueurGraphique joueur = new JoueurGraphique(grilleDefense, grilleTirs, nom);
+		
+		// A VALIDER !
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -37,5 +58,5 @@ public class FenetreJoueur extends JFrame {
 
 		setContentPane(contentPane);
 	}
-
+	
 }
