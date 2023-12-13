87b40f072a2a3eb01e1844cb8885dc01ea17280d
IliasKhms <152298532+IliasKhms@users.noreply.github.com>
2023-12-13 12:25:06 +0100

    edit

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/GrilleNavaleGraphique.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/GrilleNavaleGraphique.java
@@ -4,14 +4,19 @@ public class GrilleNavaleGraphique extends GrilleNavale {
 	private GrilleGraphique grille;
 	
 	public GrilleNavaleGraphique(int taille) {
-		
+		super(taille,0);
+		grille = new GrilleGraphique(taille);
 	}
 	
 	public GrilleGraphique getGrilleGraphique() {
+		return this.grille;
 		
 	}
 	
 	public boolean ajouteNavire(Navire n) {
+		if(super.ajouteNavire(n))
+				n.colorie(n.getDebut(),n.getFin(),color.GREEN);
+		return true;
 		
 	}
 	
