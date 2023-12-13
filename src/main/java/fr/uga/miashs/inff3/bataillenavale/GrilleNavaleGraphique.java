d738784b9054a7d0091f0673cc6151f9019b8ddd
IliasKhms <152298532+IliasKhms@users.noreply.github.com>
2023-12-13 12:47:12 +0100

    6 navires

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/GrilleNavaleGraphique.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/GrilleNavaleGraphique.java
@@ -1,11 +1,13 @@
 package fr.uga.miashs.inff3.bataillenavale;
 
+import java.awt.Color;
+
 public class GrilleNavaleGraphique extends GrilleNavale {
 	private GrilleGraphique grille;
 	
 	public GrilleNavaleGraphique(int taille) {
 
-		super(taille,0);
+		super(taille,6);
 		grille = new GrilleGraphique(taille);
 
 	}
@@ -16,18 +18,24 @@ public class GrilleNavaleGraphique extends GrilleNavale {
 	}
 	
 	public boolean ajouteNavire(Navire n) {
-
-		if(super.ajouteNavire(n))
-				n.colorie(n.getDebut(),n.getFin(),color.GREEN);
-		return true;
-		
-
 		//Spécialisation de la méthode héritée de GrilleNavale. Les cases correspondant au navire ajouté doivent être coloriées en Color.GREEN. 
-
+		if(super.ajouteNavire(n))
+				grille.colorie(n.getDebut(),n.getFin(),Color.GREEN);
+		return true;
 	}
 	
 	public boolean recoitTir(Coordonnee c) {
 		//Spécialisation de la méthode héritée de GrilleNavale. La case correspondant au tir doit être coloriée en Color.RED si le tir a touché un navire ou en Color.BLUE s'il est à l'eau.
+		for (int i = 0; i<super.getNbNavires();i++) {
+			if(super.recoitTir(c) && super.estTouche(c))
+				grille.colorie(.getDebut(),n.getFin(),Color.RED);
+			else if (super.recoitTir(c) && super.estALEau(c))
+				grille.colorie(n.getDebut(),n.getFin(),Color.RED);
+				
+		}
+			
+			
+	
 	}
 
 }
