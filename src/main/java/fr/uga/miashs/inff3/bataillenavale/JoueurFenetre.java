e337ea60174f0d1b8bc42d7e8839215f1fb0108a
honopio <honorine.piolat@gmail.com>
2023-12-13 11:59:38 +0100

    nvl classe JoueurFenetre

---
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurFenetre.java
@@ -0,0 +1,41 @@
+package fr.uga.miashs.inff3.bataillenavale;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class JoueurFenetre extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JoueurFenetre frame = new JoueurFenetre();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JoueurFenetre() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
