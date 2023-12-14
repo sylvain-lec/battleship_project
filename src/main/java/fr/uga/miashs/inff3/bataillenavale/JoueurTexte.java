7ca9da2b7afe150ff38725ade5f430d4ba829749
honopio <honorine.piolat@gmail.com>
2023-12-14 10:37:01 +0100

    chgt print retourAtk et retourDef

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurTexte.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/JoueurTexte.java
@@ -21,9 +21,9 @@ public class JoueurTexte extends JoueurAvecGrille {
 		if (etat==TOUCHE)
 			System.out.println(super.getNom()+", vous avez touché un navire en "+ c);
 		if (etat==COULE)
-			System.out.println(super.getNom()+", vous avez coulé un navire en "+ c);
+			System.out.println(super.getNom()+", tir en "+c+" : un navire a coulé");
 		if (etat==A_L_EAU)
-			System.out.println(super.getNom()+", vous avez tiré dans l'eau en" + c);
+			System.out.println(super.getNom()+", vous avez tiré dans l'eau en " + c);
 	}
 	
 	protected void retourDefense(Coordonnee c, int etat) {
@@ -31,11 +31,11 @@ public class JoueurTexte extends JoueurAvecGrille {
 		if (etat==GAMEOVER)
 			System.out.println("Vous avez perdu :(");
 		if (etat==TOUCHE)
-			System.out.println(super.getNom()+", un navire a été touché en" + c);
+			System.out.println(super.getNom()+", un navire a été touché en " + c);
 		if (etat==COULE)
-			System.out.println(super.getNom()+"tir en : "+c+", un navire a coulé");
+			System.out.println(super.getNom()+", tir en "+c+" : un navire a coulé");
 		if (etat==A_L_EAU)
-			System.out.println(super.getNom()+", tout va bien yayyy" + c);
+			System.out.println(super.getNom()+", tir en "+c+" :  tout va bien yayyy");
 	}
 	
 	public Coordonnee choixAttaque() {
